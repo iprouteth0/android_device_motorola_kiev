eecc765a8ea48eb14d438be5d4aedc3076bcf1af
iprouteth0 <jmrandall05@gmail.com>
2021-11-30 13:39:16 +0100

    add files

---
+++ bluetooth/include/bdroid_buildcfg.h
@@ -0,0 +1,58 @@
+/*
+ *
+ *  Copyright (c) 2013, The Linux Foundation. All rights reserved.
+ *  Not a Contribution, Apache license notifications and license are retained
+ *  for attribution purposes only.
+ *
+ * Copyright (C) 2012 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _BDROID_BUILDCFG_H
+#define _BDROID_BUILDCFG_H
+
+#include <stdint.h>
+#include <string.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+int property_get(const char *key, char *value, const char *default_value);
+#ifdef __cplusplus
+}
+#endif
+
+static inline const char* BtmGetDefaultName()
+{
+
+    char product_model[92];
+    property_get("ro.product.model", product_model, "");
+
+    if (strstr(product_model, "motorola one 5G ace"))
+        return "Motorola One 5G Ace";
+    if (strstr(product_model, "moto g 5G"))
+        return "Moto G 5G";
+
+    // Fallback to Moto Generic
+    return "Motorola";
+}
+
+#define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
+// Disables read remote device feature
+#define BTM_WBS_INCLUDED TRUE
+#define BTIF_HF_WBS_PREFERRED TRUE
+
+#define BLE_VND_INCLUDED   TRUE
+#endif
+
